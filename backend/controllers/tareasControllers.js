7d12f6207972fd015464d2688adac1f280a9cb6b
unknown <fasuarezd@gmail.com>
2024-06-25 21:11:16 -0600

    Mi primer ruta

---
+++ backend/controllers/tareasControllers.js
@@ -0,0 +1,7 @@
+const getTareas = (req, res) => {
+    res.status(200).json({ messsage: 'getTareas' })
+}
+
+module.exports = {
+    getTareas
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hola mundo")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/tareas', require('./routes/tareasRoutes'))
+
+app.listen(port, () => console.log(`Server iniciado en el puerto ${port}`))
