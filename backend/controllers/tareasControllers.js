72b35bf5dd4739e0906ea56db954af477d5313e3
unknown <fasuarezd@gmail.com>
2024-06-27 19:52:10 -0600

    MongoDB Connected

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,18 +1,26 @@
-const getTareas = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTareas = asyncHandler(async (req, res) => {
     res.status(200).json({ messsage: 'getTareas' })
-}
+})
 
-const crearTareas = (req, res) => {
+const crearTareas = asyncHandler(async (req, res) => {
+
+    if (!req.body.texto) {
+        res.status(400)
+        throw new Error("Favor de escribir un texto")
+    }
+
     res.status(201).json({ messsage: 'crearTareas' })
-}
+})
 
-const updateTareas = (req, res) => {
+const updateTareas = asyncHandler(async (req, res) => {
     res.status(200).json({ messsage: `Modificar la tarea ${req.params.id}` })
-}
+})
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler(async (req, res) => {
     res.status(200).json({ messsage: `Eliminar la tarea ${req.params.id}` })
-}
+})
 
 module.exports = {
     getTareas,
